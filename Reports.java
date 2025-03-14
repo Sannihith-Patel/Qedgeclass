d5b71cbf7e4c7b09d07e9bf3fdd944b54c78c002
Sannihith-Patel <perukasannihith@gmail.com>
2025-03-14 14:03:23 +0530

    Update Reports.java

--- Reports.java
+++ Reports.java
@@ -2,3 +2,14 @@ Devops engineer -Sannihith
   Created a new file in central repository
 
   #Changes made on 14th march to check CI
+  public class Calculator {
+    // Method to add two numbers
+    public static int addNumbers(int a, int b) {
+        return a + b;
+    }
+
+    // Main method to test the function
+    public static void main(String[] args) {
+        int result = addNumbers(5, 10);
+        System.out.println("Sum: " + result);  // Output: Sum: 15
+
